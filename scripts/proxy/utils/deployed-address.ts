e3c7002e3cc92e4999f908a017ecd959ca1ff9c6
root <xdgbikm@outlook.com>
2024-09-08 14:01:18 +0000

    first commit

---
+++ scripts/proxy/utils/deployed-address.ts
@@ -0,0 +1,4 @@
+export const SWTRProxy = '0x8034cAAA0aDDd1777fa73A513bC053c71AE20Ae0'
+export const ProxyAdmin = '0xA50EA0547b6A0A4243634F5c49EccE8E7815E109'
+export const SWTRImplementation = '0xd75f340182beEcA41A8b5816597cAe1597bF0c5e'
+    
